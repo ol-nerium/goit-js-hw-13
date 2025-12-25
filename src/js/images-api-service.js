3052245122ef63ce8141e9d08d7232c86abab48c
al-burlachenko <alexandrburl@gmail.com>
2025-12-26 00:38:40 +0200

    refactoring

--- src/js/images-api-service.js
+++ src/js/images-api-service.js
@@ -21,25 +21,12 @@ export default class ImageCardsService {
 
   async fetchCards() {
     const url = `${BASE_URL}?key=${parameters.key}&q=${this.searchQuery}&image_type=${parameters.image_type}&orientation=${parameters.orientation}&safesearch=${parameters.safesearch}&per_page=${parameters.per_page}&page=${this.page}`;
-
-    // return axios.get(url)
-    //   .then(response => response)
-    //   .then(response => {
-    //     return response.data;
-    //   })
-    //   .then(pictures => {
-    //     return pictures;
-    //   });
-
     const response = await axios.get(url);
-    const pictures = await response.data;
-
-    return pictures;
+    return response.data;
   }
 
   increment() {
     this.page += 1;
-    // console.log(this.page);
   }
 
   resetPage() {
@@ -49,6 +36,7 @@ export default class ImageCardsService {
   get query() {
     return this.searchQuery;
   }
+
   set query(newQuery) {
     this.searchQuery = newQuery;
   }
